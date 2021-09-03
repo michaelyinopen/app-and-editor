044ecef93f30f2bedd8a4b281ff17f57d2d060f3
michaelyinopen <michaelyinopen@gmail.com>
2021-09-03 20:26:39 +1000

    notification display

--- src/Notifications.tsx
+++ src/Notifications.tsx
@@ -1,11 +1,12 @@
 import { useAppDispatch, useAppSelector } from "./store"
 import { addNotification, clearNotifications } from "./store/actions"
+import styles from './Notifications.module.css';
 
 export const Notifications = () => {
   const dispatch = useAppDispatch()
   const notifications = useAppSelector(state => state.notifications)
   return (
-    <details>
+    <details style={{ position: 'fixed', bottom: 0, borderStyle: 'ridge', display: 'block', backgroundColor: 'white' }}>
       <summary>
         <span style={{ userSelect: 'none', fontSize: '1.2rem' }}>
           Notifications ({notifications.length})
@@ -21,35 +22,21 @@ export const Notifications = () => {
       >
         add notification
       </button>
-      <ol
+      <div
         style={{
-          display: 'flex',
-          flexDirection: 'column',
-          flexWrap: 'wrap',
-          maxHeight: 240,
-          alignContent: 'flex-start',
-          listStyleType: 'none',
-          marginBlockStart: 0,
-          marginBlockEnd: 0,
-          marginInlineStart: 0,
-          marginInlineEnd: 0,
-          paddingInlineStart: 0,
+          display: 'grid',
+          gridAutoFlow: 'column',
+          gridTemplateRows: 'repeat(10, auto)',
         }}
       >
         {notifications.map((n, index) => (
-          <li
-            key={index}
-            style={{
-              display: 'block',
-              margin: '0 5px',
-              borderStyle: 'outset',
-              maxWidth: 200
-            }}
+          <div
+            className={styles.notificationItem}
           >
             {index + 1}. {n}
-          </li>
+          </div>
         )).reverse()}
-      </ol>
+      </div>
     </details>
   )
 }
