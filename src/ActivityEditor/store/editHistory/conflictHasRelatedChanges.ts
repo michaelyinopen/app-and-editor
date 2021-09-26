d76aca0bfb834fbb20c94d57a4e5aa352212e997
michaelyinopen <michaelyinopen@gmail.com>
2021-09-27 06:49:42 +1000

    conflictHasRelatedChanges account for add/remove ride and update item property

--- src/ActivityEditor/store/editHistory/conflictHasRelatedChanges.ts
+++ src/ActivityEditor/store/editHistory/conflictHasRelatedChanges.ts
@@ -1,4 +1,7 @@
+import { numberOfSlashes } from './StepCommon'
 import {
+  CollectionAddChange,
+  CollectionRemoveChange,
   FieldChange,
   Operation,
   Step,
@@ -9,7 +12,40 @@ function hasRelatedChanges(
   aChanges: FieldChange[],
   bChanges: FieldChange[]
 ): boolean {
-  return aChanges.some(ac => bChanges.some(bc => bc.path === ac.path))
+  if (aChanges.some(ac => bChanges.some(bc => bc.path === ac.path))) {
+    return true
+  }
+  // add or remove conflicts with update ride property
+  const addOrRemoveRideAChanges = aChanges.filter(ac =>
+    ac.path === '/rides/ids'
+    && (ac.collectionChange?.type === 'add' || ac.collectionChange?.type === 'remove')
+  )
+  for (const aChange of addOrRemoveRideAChanges) {
+    const aRideId = (aChange.collectionChange as (CollectionAddChange | CollectionRemoveChange)).id
+    if (bChanges.some(bc =>
+      bc.path.startsWith(`/rides/entities/${aRideId}`)
+      && numberOfSlashes(bc.path) > 3
+    )) {
+      return true
+    }
+  }
+  // edit ride property conflicts with add or remove
+  const editRidePropertyAChanges = aChanges.filter(ac =>
+    ac.path.startsWith('/rides/entities/')
+    && numberOfSlashes(ac.path) > 3
+  )
+  for (const aChange of editRidePropertyAChanges) {
+    const indexOf3rdSlash = '/rides/entities/'.length
+    const indexOf4thSlash = aChange.path.indexOf('/', indexOf3rdSlash + 1)
+    const aRideId = aChange.path.substring('/rides/entities/'.length + 1, indexOf4thSlash)
+    if (bChanges.some(bc =>
+      bc.collectionChange?.type === 'remove'
+      && (bc.collectionChange as (CollectionRemoveChange)).id === aRideId
+    )) {
+      return true
+    }
+  }
+  return false
 }
 
 export function conflictHasRelatedChanges(operation: Operation, step: Step): boolean {
