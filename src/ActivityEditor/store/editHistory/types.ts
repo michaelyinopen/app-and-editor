c43a32a00a321a770c43c43a30671197ffa5a435
michaelyinopen <michaelyinopen@gmail.com>
2021-10-02 16:17:53 +1000

    comment

--- src/ActivityEditor/store/editHistory/types.ts
+++ src/ActivityEditor/store/editHistory/types.ts
@@ -34,7 +34,7 @@ export type Operation = {
   type: 'edit' | 'merge' | 'conflict' | 'reverse local',
   fieldChanges: FieldChange[],
   conflictName?: string,
-  conflictApplied?: boolean, // need this to preserve the selection when toggling 'merge' and 'discard local changes',
+  conflictApplied?: boolean, // need this to preserve the selection when toggling 'merge' and 'discard local changes'
   applied: boolean,
 }
 
